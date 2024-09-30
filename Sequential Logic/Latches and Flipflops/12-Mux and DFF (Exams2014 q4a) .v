0d42d0625874d6f409ec810aaaa7a704b4a8a19b
Lathikashree1999 <126261663+Lathikashree1999@users.noreply.github.com>
2024-09-30 15:17:53 +0530

    12-Mux and DFF (Exams2014 q4a) .v

--- Sequential Logic/Latches and Flipflops/12-Mux and DFF (Exams2014 q4a) .v
+++ Sequential Logic/Latches and Flipflops/12-Mux and DFF (Exams2014 q4a) .v
@@ -9,7 +9,7 @@ module top_module (
         case (con)
             2'b00 : Q <= Q;
             2'b01 : Q <= R;
-	          2'b10 : Q <= w;
+	    2'b10 : Q <= w;
             2'b11 : Q <= R;        
         endcase    
     end
